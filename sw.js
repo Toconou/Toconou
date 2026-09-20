e4163fbb168b68d66713c5696f3fbd213a144e53
Toconou <favibaudouin56@gmail.com>
2026-09-20 23:00:12 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,13 @@
-// sw.js - Toconou PWA comme WeChat
+// sw.js - Toconou PWA comme WeChat FINAL
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open('toconou-v1').then(c => 
-      c.addAll(['./', './index.html', './manifest.json', './style.css', './app.js'])
-    )
+    caches.open('toconou-v1').then(c => c.addAll(['./', './index.html', './manifest.json', './app.js']))
   );
   self.skipWaiting();
 });
-
 self.addEventListener('activate', e => {
   e.waitUntil(self.clients.claim());
 });
-
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(r => r || fetch(e.request))
